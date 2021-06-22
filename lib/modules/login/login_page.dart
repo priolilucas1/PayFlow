b900a786288136b4b8ee11505dc0549d9cde4c5c
priolilucas1 <priolilucas1@gmail.com>
2021-06-22 11:21:33 -0300

    Login Page UI Done

---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              height: size.height * 0.33,
+              width: size.width,
+              color: AppColors.primary,
+            ),
+            Positioned(
+              top: 50,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                AppImages.person,
+                width: 208,
+                height: 350,
+              ),
+            ),
+            Positioned(
+              bottom: size.height * 0.08,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 70,
+                      right: 70,
+                      top: 30,
+                      bottom: 50,
+                    ),
+                    child: Text(
+                      'Organize seus boletos em um só lugar',
+                      textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 20, left: 40, right: 40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        print('Clicou');
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
