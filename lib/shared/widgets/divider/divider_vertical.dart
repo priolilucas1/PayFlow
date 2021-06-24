02380e6bd98740052904cc86cb3e5d8c3b88f928
priolilucas1 <priolilucas1@gmail.com>
2021-06-24 15:14:06 -0300

    Custom Divider Vertical Done

--- lib/shared/widgets/divider/divider_vertical.dart
+++ lib/shared/widgets/divider/divider_vertical.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 class DividerVertical extends StatelessWidget {
+  const DividerVertical({required this.height});
+
+  final double height;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 1,
-      height: double.maxFinite,
+      height: height,
       color: AppColors.stroke,
     );
   }
