5558b23a9f647cec489878f8017087b6cd451eef
priolilucas1 <priolilucas1@gmail.com>
2021-06-25 10:45:22 -0300

    Insert Boleto Page Created

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/input_text/custom_input_text.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:payflow/shared/widgets/set_label_buttons.dart/set_label_buttons.dart';
 
 class InsertBoletoPage extends StatelessWidget {
   const InsertBoletoPage({Key? key}) : super(key: key);
@@ -6,8 +11,58 @@ class InsertBoletoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
-        title: Text('Insert Boleto Page'),
+        elevation: 0,
+        backgroundColor: AppColors.background,
+        leading: BackButton(
+          color: AppColors.grey,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 93, vertical: 24),
+              child: Text(
+                'Preencha os dados do boleto',
+                style: TextStyles.titleBoldHeading,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            CustomInputText(
+              label: 'Nome do boleto',
+              icon: Icons.description_outlined,
+              onChanged: (value) {},
+            ),
+            CustomInputText(
+              label: 'Vencimento',
+              icon: FontAwesomeIcons.timesCircle,
+              onChanged: (value) {},
+            ),
+            CustomInputText(
+              label: 'Valor',
+              icon: FontAwesomeIcons.wallet,
+              onChanged: (value) {},
+            ),
+            CustomInputText(
+              label: 'Código',
+              icon: FontAwesomeIcons.barcode,
+              onChanged: (value) {},
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SetLabelButtons(
+        primaryLabel: 'Cancelar',
+        primaryOnPressed: () {
+          Navigator.pop(context);
+        },
+        secondaryLabel: 'Cadastrar',
+        secondaryOnPressed: () {},
+        enableSecondaryColor: true,
       ),
     );
   }
